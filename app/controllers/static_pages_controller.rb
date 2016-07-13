a11e77093fde649e91443f725a5be6a7ff7ead08
Cody Loyd <codyloyd@gmail.com>
2016-07-12 19:12:27 -0500

    got classes and dropdowns going

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,6 @@ class StaticPagesController < ApplicationController
   def home
     @note = StudentNote.new
     @title = "Anecdotal Notes"
+    @student_groups = current_user.student_groups
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'students/show'
-
   devise_for :users
   resources 'users'
   resources 'student_group'
   resources 'students'
   resources 'student_notes'
   root 'static_pages#home'
+  get '/:student_group', to: 'static_pages#home', as: :student_group_homepage
 end
